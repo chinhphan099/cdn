7c156b4edb5dc00198895c3e6fecb4f590640eee
Chinh Phan <chinh.phan@dfo.global>
2019-11-26 00:03:19 +0700

    banner holiday

--- Holiday Banner/holidaybanner.page.js
+++ Holiday Banner/holidaybanner.page.js
@@ -66,7 +66,42 @@
         }
     }
 
-    document.addEventListener('load', function() {
+    function adjustPosition(bottomValue) {
+        const holidayBanners = _qAll('.holiday-banner');
+        Array.prototype.slice.call(holidayBanners).forEach(holidaybanner => {
+            const bottom = bottomValue + 'px';
+            if(bottomValue === 0 && holidaybanner.classList.contains('has-floating-btn')) {
+                holidaybanner.classList.remove('has-floating-btn');
+            }
+            else if(bottomValue !== 0 && !holidaybanner.classList.contains('has-floating-btn')) {
+                holidaybanner.classList.add('has-floating-btn');
+            }
+            if(holidaybanner.style.bottom !== bottom) {
+                holidaybanner.style.bottom = bottom;
+            }
+        });
+    }
+
+    function checkFloating() {
+        if(!!_q('.floating-bar').classList.contains('floating-visible')) {
+            adjustPosition(_q('.floating-bar').clientHeight);
+        }
+        else {
+            adjustPosition(0);
+        }
+    }
+
+    window.addEventListener('load', function() {
+        if(!!_q('body').classList.contains('edit_mode') || !!_q('body').classList.contains('editMode')) {
+            return;
+        }
         init();
+        checkFloating();
+    });
+    window.addEventListener('scroll', function() {
+        if(!!_q('body').classList.contains('edit_mode') || !!_q('body').classList.contains('editMode')) {
+            return;
+        }
+        checkFloating();
     });
 })(window.utils);
