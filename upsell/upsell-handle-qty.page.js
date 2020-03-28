c057bcf29058dcc346454edbe977e551b8dcda9c
Chinh Phan <chinh.phan@dfo.global>
2020-03-28 10:36:01 +0700

    unitx, orderunitx

--- upsell/upsell-handle-qty.page.js
+++ upsell/upsell-handle-qty.page.js
@@ -4,8 +4,24 @@
         for(const unitElm of unitElms) {
             unitElm.innerText = index;
         }
+        Array.prototype.slice.call(_qAll('[class^="unitx"]')).forEach((cls) => {
+            const num = Number(cls.className.replace(/[^0-9]/g,''));
+            cls.textContent = index * num;
+        });
         --index;
         _qAll('.productRadioListItem input')[index].click();
+    }
+
+    function impletementNumberXOrderQuantity() {
+        Array.prototype.slice.call(_qAll('[class^="orderunitx"]')).forEach((cls) => {
+            let num = Number(cls.className.replace(/[^0-9]/g,'')),
+                qty = JSON.parse(utils.localStorage().get('orderInfo')).quantity;
+
+            if(!!utils.localStorage().get('doubleQuantity')) {
+                qty /= 2;
+            }
+            cls.textContent = qty * num;
+        });
     }
 
     function q(selector) {
@@ -54,6 +70,7 @@
     }
 
     function initial() {
+        impletementNumberXOrderQuantity();
     }
 
     window.addEventListener('DOMContentLoaded', () => {
