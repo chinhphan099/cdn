33d2501e925bce2dacb455e81eede4d7d7b3c374
Chinh Phan <chinh.phan@dfo.global>
2020-05-14 11:12:25 +0700

    optimized

--- w_productlist/widget_productlist_v1.js
+++ w_productlist/widget_productlist_v1.js
@@ -529,22 +529,23 @@
                 }
                 utils.events.emit('bindProductDiscountInfo', productInfo);
                 siteSetting.countryCode = countryCodeIndex || data.location.countryCode;
-                if (!countryCodeIndex) {
-                    utils.localStorage().set('countryCode', data.location.countryCode);
-                    utils.events.emit('triggerAddressForm', data.location.countryCode);
-                }
-                else {
-                    utils.localStorage().set('countryCode', countryCodeIndex);
-                }
+                // if (!countryCodeIndex) {
+                //     utils.localStorage().set('countryCode', data.location.countryCode);
+                //     utils.events.emit('triggerAddressForm', data.location.countryCode);
+                // }
+                // else {
+                //     utils.localStorage().set('countryCode', countryCodeIndex);
+                // }
+                utils.localStorage().set('countryCode', siteSetting.countryCode);
                 setTimeout(() => {
                     utils.localStorage().set('jsCurrency', window.fCurrency);
-                    utils.localStorage().set('countryCode', siteSetting.countryCode);
+                    // utils.localStorage().set('countryCode', siteSetting.countryCode);
                     utils.localStorage().set('currencyCode', productInfo.currencyCode);
                     utils.localStorage().set('ip', data.location.ip);
                     utils.localStorage().set('mainCampaignName', data.campaignName);
                     utils.localStorage().set('mainWebKey', window.siteSetting.webKey);
-                }, 1000);
-                utils.events.emit('triggerAddressForm', data.location.countryCode);
+                }, 600);
+                utils.events.emit('triggerAddressForm', siteSetting.countryCode);
                 utils.events.emit('triggerProductBannerSidebar', productInfo);
                 utils.events.emit('triggerWarranty', getSelectedProduct());
                 utils.events.emit('bindOrderPage', productInfo);
