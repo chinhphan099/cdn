c462cae8b10f4746a4b4c4b8921c4322585120a0
Chinh Phan <chinh.phan@dfo.global>
2020-12-29 11:10:30 +0700

    adjust-product-order.page.js

--- js/adjust-product-order.page.js
+++ js/adjust-product-order.page.js
@@ -4,24 +4,29 @@
     }
 
     function init() {
-        if (utils.getQueryParameter('temp') === 'hcvr') {
-            _q('.productRadioListItem.item-1:not(.hidden) .js-unitDiscountRate').click();
-            _q('body').classList.add('hcvr', 'wasteClick');
-        }
+        try {
+            if (utils.getQueryParameter('temp') === 'hcvr') {
+                _q('.productRadioListItem.item-1:not(.hidden) .js-unitDiscountRate').click();
+                _q('body').classList.add('hcvr', 'wasteClick');
+            }
 
-        if (utils.getQueryParameter('temp') === 'haov') {
-            _q('.productRadioListItem.item-3:not(.hidden) .js-unitDiscountRate').click();
-            _q('body').classList.add('haov', 'wasteClick');
-        }
+            if (utils.getQueryParameter('temp') === 'haov') {
+                _q('.productRadioListItem.item-3:not(.hidden) .js-unitDiscountRate').click();
+                _q('body').classList.add('haov', 'wasteClick');
+            }
 
-        if (utils.getQueryParameter('temp') === 'hcvr3') {
-            _q('.productRadioListItem.item-2:not(.hidden) .js-unitDiscountRate').click();
-            _q('body').classList.add('hcvr3', 'wasteClick');
-        }
+            if (utils.getQueryParameter('temp') === 'hcvr3') {
+                _q('.productRadioListItem.item-2:not(.hidden) .js-unitDiscountRate').click();
+                _q('body').classList.add('hcvr3', 'wasteClick');
+            }
 
-        if (utils.getQueryParameter('temp') === 'haov3') {
-            _q('.productRadioListItem.item-2:not(.hidden) .js-unitDiscountRate').click();
-            _q('body').classList.add('haov3', 'wasteClick');
+            if (utils.getQueryParameter('temp') === 'haov3') {
+                _q('.productRadioListItem.item-2:not(.hidden) .js-unitDiscountRate').click();
+                _q('body').classList.add('haov3', 'wasteClick');
+            }
+        }
+        catch (e) {
+            console.log(e);
         }
     }
 
