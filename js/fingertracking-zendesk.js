0e1105374498603ee92ed368dd641b19cd387716
Chinh Phan <chinh.phan@dfo.global>
2021-05-19 11:09:40 +0700

    try catch

--- js/fingertracking-zendesk.js
+++ js/fingertracking-zendesk.js
@@ -5,16 +5,20 @@
   window.addEventListener('load', function() {
     try {
       (function(w, d, s, l, i) {
-        if (!i) return;
-        w[l] = w[l] || [];
-        w[l].push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
+        try {
+          if (!i) return;
+          w[l] = w[l] || [];
+          w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+          });
+          var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+          j.async = true;
+          j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        } catch (e) {
+          console.log(e);
+        }
       })(window, document, 'script', 'dataLayer', window.gtmId);
 
       (function(w, d, b, s, t) {
