04260923b8f1294da775de557cf8171f5d39da60
Chinh Phan <chinh.phan@dfo.global>
2021-05-19 11:06:47 +0700

    Tab

--- js/fingertracking-zendesk.js
+++ js/fingertracking-zendesk.js
@@ -4,37 +4,37 @@
 
   window.addEventListener('load', function() {
     try {
-    (function(w, d, s, l, i) {
-      if (!i) return;
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', window.gtmId);
+      (function(w, d, s, l, i) {
+        if (!i) return;
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', window.gtmId);
 
-    (function(w, d, b, s, t) {
-      try {
-        if (!w._CTR_TRACKING_ID) return;
-        w._CTR_CUSTOM_DATA = {
-          siteName: w.location.hostname,
-          pageName: d.querySelector(t).textContent || '',
-          pageType: '',
-          campaignName: '',
-          campaignWebKey: ''
-        };
-        var j = d.createElement(s);
-        j.src = '//d16hdrba6dusey.cloudfront.net/sitecommon/js/commons/ctrwow_analytics.v3.pro.min.js'
-        j.defer = true;
-        d.querySelector(b).appendChild(j);
-      } catch(err) {
-        console.log('Bind fingerprint error: ', err);
-      }
-    })(window, document, 'body', 'script', 'title');
+      (function(w, d, b, s, t) {
+        try {
+          if (!w._CTR_TRACKING_ID) return;
+          w._CTR_CUSTOM_DATA = {
+            siteName: w.location.hostname,
+            pageName: d.querySelector(t).textContent || '',
+            pageType: '',
+            campaignName: '',
+            campaignWebKey: ''
+          };
+          var j = d.createElement(s);
+          j.src = '//d16hdrba6dusey.cloudfront.net/sitecommon/js/commons/ctrwow_analytics.v3.pro.min.js'
+          j.defer = true;
+          d.querySelector(b).appendChild(j);
+        } catch(err) {
+          console.log('Bind fingerprint error: ', err);
+        }
+      })(window, document, 'body', 'script', 'title');
     } catch (e) {
       console.log(e)
     }
