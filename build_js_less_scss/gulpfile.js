2937cb582b732f2385366518aa2675904ca73636
Chinh Phan <chinhphan099@gmail.com>
2021-10-04 10:28:10 +0700

    Popup

--- build_js_less_scss/gulpfile.js
+++ build_js_less_scss/gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { task, watch, src, dest, parallel, series } = require('gulp'),
+  less = require('gulp-less'),
+  sass = require('gulp-sass')(require('sass')),
   jshint = require('gulp-jshint'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
@@ -8,17 +10,28 @@ const { task, watch, src, dest, parallel, series } = require('gulp'),
   webserver = require('gulp-webserver'),
   gutil = require('gulp-util'),
   // ip = require('ip'),
+  autoprefixer = require('gulp-autoprefixer'),
   babel = require('gulp-babel');
 
 // Source folder configuration
 const SRC = {};
-SRC.root = './';
+SRC.root = './app/';
 SRC.js = SRC.root + 'js/*.js';
+SRC.less = SRC.root + 'less/';
+SRC.scss = SRC.root + 'scss/';
+SRC.assets = SRC.root + 'assets/';
+
+const FILES = {
+  assets: SRC.assets + '**/*',
+  less: SRC.less + '*.less',
+  scss: SRC.scss + '*.scss',
+};
 
 // Output directories
 const PUB = {};
 PUB.root = './public/';
 PUB.js = PUB.root + 'js/';
+PUB.css = PUB.root + 'css/';
 
 task('scripts', () =>
   src([SRC.js])
@@ -52,12 +65,50 @@ task('releaseScripts', () =>
     .pipe(dest(PUB.js))
 );
 
+task('less', () =>
+  src(FILES.less)
+    .pipe(less().on('error', function (err) {
+      let displayErr = gutil.colors.red(err.message);
+      gutil.log(displayErr);
+      this.emit('end');
+    }))
+    .pipe(autoprefixer('last 3 versions'))
+    .pipe(dest(PUB.css))
+  //.pipe(cssmin())
+  //.pipe(rename({ suffix: '.min' }))
+  //.pipe(dest(PUB.css))
+);
+task('scss', () =>
+  src(FILES.scss)
+    .pipe(sass().on('error', function (err) {
+      let displayErr = gutil.colors.red(err.message);
+      gutil.log(displayErr);
+      this.emit('end');
+    }))
+    .pipe(autoprefixer('last 3 versions'))
+    .pipe(dest(PUB.css))
+  //.pipe(cssmin())
+  //.pipe(rename({ suffix: '.min' }))
+  //.pipe(dest(PUB.css))
+);
+
+task('copyAssets', () =>
+  src(FILES.assets).pipe(dest(PUB.root))
+);
+
 task('watch', (done) => {
   watch([SRC.js], series('scripts'));
+  watch(FILES.assets, series('copyAssets'));
+  watch(FILES.less, series('less'));
+  watch(FILES.scss, series('scss'));
   done();
 });
+
 task('watchrl', (done) => {
   watch([SRC.js], series('releaseScripts'));
+  watch(FILES.assets, series('copyAssets'));
+  watch(FILES.less, series('less'));
+  watch(FILES.scss, series('scss'));
   done();
 });
 
@@ -77,11 +128,11 @@ task('webserver', (done) => {
 });
 
 task('build',
-  parallel('scripts', 'watch')
+  parallel('scripts', 'less', 'scss', 'copyAssets', 'watch')
 );
 task('default',
   series('clean', 'build', 'webserver')
 );
 task('release',
-  series('clean', 'releaseScripts', 'watchrl', 'webserver')
+  series('clean', 'releaseScripts', 'less', 'scss', 'copyAssets', 'watchrl', 'webserver')
 );
